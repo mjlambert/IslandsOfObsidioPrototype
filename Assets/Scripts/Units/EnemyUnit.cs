c276880d989f76f17cca9235cf239d8645b53f16
mjlambert <mjlambert55@gmail.com>
2016-05-14 20:56:53 +0800

    add ememey unit navigation code

--- Assets/Scripts/Units/EnemyUnit.cs
+++ Assets/Scripts/Units/EnemyUnit.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class EnemyUnit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private NavMeshAgent navMeshAgent;
+
+    // Use this for initialization
+    void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Vector3 nextTarget;
+        NavMeshPath path = new NavMeshPath();
+
+        switch (other.gameObject.name)
+        {
+            case "NorthEastTrigger":
+                nextTarget = GameObject.Find("NorthWestTrigger").GetComponent<Transform>().position;
+                break;
+            case "NorthWestTrigger":
+                nextTarget = GameObject.Find("SouthWestTrigger").GetComponent<Transform>().position;
+                break;
+            case "SouthWestTrigger":
+                nextTarget = GameObject.Find("SouthEastTrigger").GetComponent<Transform>().position;
+                break;
+            case "SouthEastTrigger":
+                nextTarget = GameObject.Find("NorthEastTrigger").GetComponent<Transform>().position;
+                break;
+            default:
+                nextTarget = GameObject.Find("NorthWestTrigger").GetComponent<Transform>().position;
+                break;
+        }
+
+        navMeshAgent.CalculatePath(nextTarget, path);
+        navMeshAgent.SetPath(path);
+    }
 }
