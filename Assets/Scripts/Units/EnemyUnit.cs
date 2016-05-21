f01b32ee4d99f623ed157216e2c09fd38499e598
Jacob Hilsley <Jacob Hilsley>
2016-05-21 17:18:44 +0800

    Different enemy movement script

--- Assets/Scripts/Units/EnemyUnit.cs
+++ Assets/Scripts/Units/EnemyUnit.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class EnemyUnit : MonoBehaviour {
 
+    /*
     private NavMeshAgent navMeshAgent;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         navMeshAgent = GetComponent<NavMeshAgent>();
         NavMeshPath path = new NavMeshPath();
         navMeshAgent.CalculatePath(GameObject.Find("SouthWestTrigger").GetComponent<Transform>().position, path);
         navMeshAgent.SetPath(path);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -44,5 +47,71 @@ public class EnemyUnit : MonoBehaviour {
 
         navMeshAgent.CalculatePath(nextTarget, path);
         navMeshAgent.SetPath(path);
+    }
+    */
+
+    NavMeshAgent navMeshAgent; //reference to nav mesh agent
+    Transform northWest, southWest, southEast, northEast; //reference to waypoint locations
+    bool NW_Hit, SW_Hit, SE_Hit, NE_Hit; //refence for waypoint collision
+    
+    void Awake()
+    {
+        northWest = GameObject.FindGameObjectWithTag("WaypointNW").transform;
+        southWest = GameObject.FindGameObjectWithTag("WaypointSW").transform;
+        southEast = GameObject.FindGameObjectWithTag("WaypointSE").transform;
+        northEast = GameObject.FindGameObjectWithTag("WaypointNE").transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        //SE_Hit = true;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == northWest)
+        {
+            NW_Hit = true;
+            NE_Hit = false;
+        }
+
+        if (other.gameObject == southWest)
+        {
+            SW_Hit = true;
+            NE_Hit = false;
+        }
+
+        if (other.gameObject == southEast)
+        {
+            SE_Hit = true;
+            SW_Hit = false;
+        }
+
+        if (other.gameObject == northEast)
+        {
+            NE_Hit = true;
+            SE_Hit = false;
+        }
+    }
+
+    void Update()
+    {
+        if (NW_Hit == true)
+        {
+            navMeshAgent.SetDestination(southWest.position);
+        }
+
+        if (SW_Hit == true)
+        {
+            navMeshAgent.SetDestination(southEast.position);
+        }
+
+        if (SE_Hit == true)
+        {
+            navMeshAgent.SetDestination(northEast.position);
+        }
+
+        if (NE_Hit == true)
+        {
+            navMeshAgent.SetDestination(northWest.position);
+        }
+
     }
 }
