a362ed32ad61d1c314f8d26d4ad1046c3ab32c8a
mjlambert <mjlambert55@gmail.com>
2016-05-25 21:33:40 +0800

    Get waypoint system working + tidy up scripts

--- Assets/Scripts/Units/EnemyUnitController.cs
+++ Assets/Scripts/Units/EnemyUnitController.cs
@@ -3,115 +3,16 @@ using System.Collections;
 
 public class EnemyUnitController : MonoBehaviour {
 
-    /*
-    private NavMeshAgent navMeshAgent;
+    NavMeshAgent navMeshAgent;
 
-    // Use this for initialization
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        NavMeshPath path = new NavMeshPath();
-        navMeshAgent.CalculatePath(GameObject.Find("SouthWestTrigger").GetComponent<Transform>().position, path);
-        navMeshAgent.SetPath(path);
+        SetDestination(Waypoint.FindWaypointPosition(1));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetDestination(Vector3 target)
     {
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        Vector3 nextTarget;
-        NavMeshPath path = new NavMeshPath();
-
-        switch (other.gameObject.name)
-        {
-            case "NorthEastTrigger":
-                nextTarget = GameObject.Find("NorthWestTrigger").GetComponent<Transform>().position;
-                break;
-            case "NorthWestTrigger":
-                nextTarget = GameObject.Find("SouthWestTrigger").GetComponent<Transform>().position;
-                break;
-            case "SouthWestTrigger":
-                nextTarget = GameObject.Find("SouthEastTrigger").GetComponent<Transform>().position;
-                break;
-            case "SouthEastTrigger":
-                nextTarget = GameObject.Find("NorthEastTrigger").GetComponent<Transform>().position;
-                break;
-            default:
-                nextTarget = GameObject.Find("NorthWestTrigger").GetComponent<Transform>().position;
-                break;
-        }
-
-        navMeshAgent.CalculatePath(nextTarget, path);
-        navMeshAgent.SetPath(path);
-    }
-    */
-
-    NavMeshAgent navMeshAgent; //reference to nav mesh agent
-    Transform northWest, southWest, southEast, northEast; //reference to waypoint locations
-    bool NW_Hit, SW_Hit, SE_Hit, NE_Hit; //refence for waypoint collision
-    
-    void Awake()
-    {
-        northWest = GameObject.FindGameObjectWithTag("WaypointNW").transform;
-        southWest = GameObject.FindGameObjectWithTag("WaypointSW").transform;
-        southEast = GameObject.FindGameObjectWithTag("WaypointSE").transform;
-        northEast = GameObject.FindGameObjectWithTag("WaypointNE").transform;
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        //SE_Hit = true;
-    }
-
-    void OnTriggerEnter (Collider other)
-    {
-        if (other.gameObject == northWest)
-        {
-            NW_Hit = true;
-            NE_Hit = false;
-        }
-
-        if (other.gameObject == southWest)
-        {
-            SW_Hit = true;
-            NE_Hit = false;
-        }
-
-        if (other.gameObject == southEast)
-        {
-            SE_Hit = true;
-            SW_Hit = false;
-        }
-
-        if (other.gameObject == northEast)
-        {
-            NE_Hit = true;
-            SE_Hit = false;
-        }
-    }
-
-    void Update()
-    {
-        if (NW_Hit == true)
-        {
-            navMeshAgent.SetDestination(southWest.position);
-        }
-
-        if (SW_Hit == true)
-        {
-            navMeshAgent.SetDestination(southEast.position);
-        }
-
-        if (SE_Hit == true)
-        {
-            navMeshAgent.SetDestination(northEast.position);
-        }
-
-        if (NE_Hit == true)
-        {
-            navMeshAgent.SetDestination(northWest.position);
-        }
-
+        navMeshAgent.SetDestination(target);
     }
 }
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+    public int order;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "EnemyUnits")
+        {
+            EnemyUnitController enemyUnitController;
+            try
+            {
+                enemyUnitController = other.GetComponentInParent<EnemyUnitController>();
+                Vector3 nextWaypointPosition = FindWaypointPosition(order + 1);
+                enemyUnitController.SetDestination(nextWaypointPosition);
+            }
+            catch (UnityException exception)
+            {
+                // TODO: Probably should throw this exception
+                Debug.Log("No EnemyUnitController script found. This object should not have the 'EnemyUnits' tag. " + exception.Message);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Find the position of a waypoint with the specified order.
+    /// </summary>
+    /// <param name="order">Order of the waypoint to find.</param>
+    /// <returns>Position of the waypoint.</returns>
+    public static Vector3 FindWaypointPosition(int order)
+    {   
+        // Search for waypoints
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Waypoints"))
+        {
+            // Try to get Waypoint script component
+            Waypoint waypointScript = null;
+            try
+            {
+                waypointScript = gameObject.GetComponent<Waypoint>();
+            }
+            catch (UnityException exception)
+            {
+                // TODO: Probably should throw this exception
+                Debug.Log("No Waypoint script found. This object should not have the 'Waypoints' tag. " + exception.Message);
+            }
+
+            // If waypoint matches the order, return its position
+            if (waypointScript != null && waypointScript.order == order)
+            {
+                return gameObject.GetComponent<Transform>().position;
+            }
+        }
+
+        // No waypoint was found, let's loop to first waypoint
+        if (order != 1)
+        {
+            return FindWaypointPosition(1);
+        }
+        else
+        {
+            Debug.Log("Could not find waypoint with order of 1. Defaulting position to (0, 0, 0)");
+            return Vector3.zero;
+        }
+    }
+
+}
