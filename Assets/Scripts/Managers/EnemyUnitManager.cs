a31fdf72a914d1fb92aba055d640d67940826b9c
mjlambert <mjlambert55@gmail.com>
2016-06-01 20:51:06 +0800

    tidy up waypoints and create enemy unit manager

---
+++ Assets/Scripts/Managers/EnemyUnitManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyUnitManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnEnemyUnit(Vector3 position)
+    {
+        Instantiate(Resources.Load(@"Prefabs/Units/EnemyUnit"), position, Quaternion.identity);
+    }
+}
