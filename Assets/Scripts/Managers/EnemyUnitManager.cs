06b23a30847eb6e460f36f1432e83c00ecda840c
mjlambert <mjlambert55@gmail.com>
2016-06-06 19:29:22 +0800

    Tidy up error handling and enemy unit spawning

--- Assets/Scripts/Managers/EnemyUnitManager.cs
+++ Assets/Scripts/Managers/EnemyUnitManager.cs
@@ -3,18 +3,87 @@ using System.Collections;
 
 public class EnemyUnitManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    /// <summary>
+    /// Location to spawn enemy units.
+    /// </summary>
+    public Vector3 enemyUnitSpawnLocation;
+
+    /// <summary>
+    /// Spawn Rate of the enemy units.
+    /// </summary>
+    public float spawnRate;
+    
+    /// <summary>
+    /// Types of enemy units.
+    /// </summary>
+    public enum EnemyUnit
+    {
+        standard
+    }
+    
+    /// <summary>
+    /// Enemy unit to spawn in current wave.
+    /// </summary>
+    private EnemyUnit currentEnemyUnit;
+
+    /// <summary>
+    /// Number of units to spawn in current wave.
+    /// </summary>
+    private int waveSpawnLimit;
+
+    /// <summary>
+    /// Enemy units spawned in current wave.
+    /// </summary>
+    private int enemyUnitsSpawned;
+
+	void Start ()
+    {
+        currentEnemyUnit = EnemyUnit.standard;
+        waveSpawnLimit = 100;
+        enemyUnitsSpawned = 0;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    // If current wave is over, stop spawning enemy units.
+        if (enemyUnitsSpawned >= waveSpawnLimit)
+        {
+            CancelInvoke("SpawnEnemyUnit");
+        }
 	}
 
-    void SpawnEnemyUnit(Vector3 position)
+    /// <summary>
+    /// Start a Wave.
+    /// </summary>
+    /// <param name="unit">Enemy unit to spawn in wave.</param>
+    /// <param name="spawnLimit">Number of enemy units to spawn.</param>
+    public void StartWave(EnemyUnit unit, int spawnLimit)
     {
-        Instantiate(Resources.Load(@"Prefabs/Units/EnemyUnit"), position, Quaternion.identity);
+        currentEnemyUnit = unit;
+        waveSpawnLimit = spawnLimit;
+        InvokeRepeating("SpawnEnemyUnit", spawnRate, spawnRate);
     }
+
+    /// <summary>
+    /// Spawns the current enemy unit at the enemy unit spawn location/
+    /// </summary>
+    private void SpawnEnemyUnit()
+    {
+        Instantiate(Resources.Load(GetUnitResource(currentEnemyUnit)), enemyUnitSpawnLocation, Quaternion.identity);
+        enemyUnitsSpawned++;
+    }
+
+    /// <summary>
+    /// Maps enemy unit type to resource location.
+    /// </summary>
+    /// <param name="unit">Enemy unit.</param>
+    private string GetUnitResource(EnemyUnit unit)
+    {
+        switch (unit)
+        {
+            case EnemyUnit.standard: return @"Prefabs/Units/EnemyUnit";
+            default: return @"Prefabs/Units/EnemyUnit";
+        }
+    }
+
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,46 +4,25 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
-    // Temporary!
-    const int waveLimit = 100;
-    int numberOfWaves = 0;
+    // Other Managers
+    EnemyUnitManager enemyUnitManager;
     
     // Use this for initialization
 	void Start ()
     {
-        InvokeRepeating("SpawnWave", 2f, 1f);
+        enemyUnitManager = GetComponentInParent<EnemyUnitManager>();
+        if (enemyUnitManager == null)
+        {
+            Debug.LogError("Enemy Unit Manager could not be found.");
+        }
+
+        enemyUnitManager.StartWave(EnemyUnitManager.EnemyUnit.standard, 100);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    if (numberOfWaves >= waveLimit)
-        {
-            CancelInvoke("SpawnWave");
-        }
+        
 	}
-
-    // This is just a test. This logic should be moved to an enemy unit manager
-    void SpawnWave()
-    {
-        numberOfWaves++;
-
-        Vector3 spawnOrigin = new Vector3(44, 0, -55);
-        List<Vector3> spawnLocations = new List<Vector3>()
-        {
-            { new Vector3(spawnOrigin.x + 2, spawnOrigin.y, spawnOrigin.z) },
-            { new Vector3(spawnOrigin.x + 1, spawnOrigin.y, spawnOrigin.z) },
-            { spawnOrigin },
-            { new Vector3(spawnOrigin.x - 1, spawnOrigin.y, spawnOrigin.z) },
-            { new Vector3(spawnOrigin.x - 2, spawnOrigin.y, spawnOrigin.z) }
-        };
-
-        int numberOfUnitsPerWave = 1;
-
-        for (int i = 0; i < numberOfUnitsPerWave; i++)
-        {
-            Instantiate(Resources.Load(@"Prefabs/Units/EnemyUnit"), spawnLocations[i], Quaternion.identity);
-        }
-    }
 
 }
